2b4399c0119ea6032ce761d82c9cf5832a6bdb33
Dennis Collinson <dennis@impulselabs.com>
2023-02-01 13:00:13 -0700

    saveing characters, saving lives

--- prost-arrow-test/build.rs
+++ prost-arrow-test/build.rs
@@ -1,7 +1,7 @@
 use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(
-        &["space_corp.proto", "version_2.proto", "version_3.proto"],
+        &["spacecorp.proto", "version_2.proto", "version_3.proto"],
         &["../protos/test"],
     )?;
     Ok(())
