9b10aa71c115e964afc1a588eeee038e3255d85c
Chang She <759245+changhiskhan@users.noreply.github.com>
2023-03-08 12:13:38 -0800

    parse the dictionary values and return with the schema

--- katniss-pb2arrow/src/schema_conversion.rs
+++ katniss-pb2arrow/src/schema_conversion.rs
@@ -1,11 +1,12 @@
 //! Convert Protobuf schema and message into Apache Arrow Schema and Tables.
 //!
 
+use std::cell::RefCell;
+use arrow_array::StringArray;
 use std::collections::{HashMap, HashSet};
 use std::io::{BufReader, Read};
 use std::path::Path;
 use std::process::Command;
-use arrow_array::StringArray;
 
 use arrow_schema::{DataType, Field, Schema, SchemaRef};
 use prost_reflect::{DescriptorPool, FieldDescriptor, MessageDescriptor};
@@ -13,17 +14,11 @@ use tempfile::NamedTempFile;
 
 use crate::{KatnissArrowError, RecordBatchConverter, Result};
 
-/// Dynamically convert protobuf messages to Arrow table or Schema.
-#[derive(Debug, Clone)]
-pub struct SchemaConverter {
-    pub(crate) descriptor_pool: DescriptorPool,
-}
-
-/// Not threadsafe
+/// Holds dictionary values for fields. Not threadsafe
 #[derive(Debug, Clone)]
 pub struct DictValuesContainer {
     /// Arrow Field.dict_id -> dictionary values
-    dictionaries: HashMap<i64, StringArray>
+    dictionaries: HashMap<i64, StringArray>,
 }
 
 impl DictValuesContainer {
@@ -35,8 +30,15 @@ impl DictValuesContainer {
     /// Add a new set of dictionary values and return dict_id
     pub fn add_dictionary(&mut self, dict_values: Vec<String>) -> i64 {
         // dict_id is 0 by default in Arrow Field so we start at 1 to distinguish
-        let new_id = self.dictionaries.keys().max().unwrap_or(&(0 as i64)).clone() + 1;
-        self.dictionaries.insert(new_id, StringArray::from_iter_values(dict_values.iter()));
+        let new_id = self
+            .dictionaries
+            .keys()
+            .max()
+            .unwrap_or(&(0 as i64))
+            .clone()
+            + 1;
+        self.dictionaries
+            .insert(new_id, StringArray::from_iter_values(dict_values.iter()));
         new_id
     }
 
@@ -46,65 +48,98 @@ impl DictValuesContainer {
     }
 }
 
-/// Convert prost FieldDescriptor to arrow Field
-fn to_arrow(f: &FieldDescriptor) -> Field {
-    let name = f.name();
-    let data_type = kind_to_type(f.kind());
-    // OneOf fields are laid out weird. Each of the oneof's appear at the top level of the
-    // message, and there's a separate oneof container that associates the oneof fields together
-    // this means we can just sort of ignore the association during schema conversion for now
-    // and pretend it's just separate arrow fields.
-    //
-    // If we're concerned about storage size (and it sounds like it won't be the case for now)
-    // we can map OneOf's to an Arrow UnionType. This essentially makes the child arrays densely
-    // packed to save space and relies on a separate offset array to restore at read-time.
-    // However I think higher level query engines tend to not deal well with UnionTypes so
-    // we should just keep the "striped" layout for now
-    if f.is_list() {
-        let item = Box::new(Field::new("item", data_type, true));
-        Field::new(name, DataType::List(item), true)
-    } else {
-        Field::new(name, data_type, true)
+/// Convert PB field to Arrow field
+#[derive(Debug, Clone)]
+pub struct FieldConverter {
+    dictionaries: DictValuesContainer,
+}
+
+impl FieldConverter {
+    pub fn new() -> Self {
+        let dictionaries = DictValuesContainer::new();
+        FieldConverter { dictionaries }
+    }
+
+    /// Convert prost FieldDescriptor to arrow Field
+    pub fn to_arrow_mut(&mut self, f: &FieldDescriptor) -> Field {
+        let name = f.name();
+        let data_type = self.kind_to_type(f.kind());
+        // OneOf fields are laid out weird. Each of the oneof's appear at the top level of the
+        // message, and there's a separate oneof container that associates the oneof fields together
+        // this means we can just sort of ignore the association during schema conversion for now
+        // and pretend it's just separate arrow fields.
+        //
+        // If we're concerned about storage size (and it sounds like it won't be the case for now)
+        // we can map OneOf's to an Arrow UnionType. This essentially makes the child arrays densely
+        // packed to save space and relies on a separate offset array to restore at read-time.
+        // However I think higher level query engines tend to not deal well with UnionTypes so
+        // we should just keep the "striped" layout for now
+        if f.is_list() {
+            let item = Box::new(Field::new("item", data_type, true));
+            Field::new(name, DataType::List(item), true)
+        } else if matches!(data_type, DataType::Dictionary(_, _)) {
+            let enum_values = f
+                .kind()
+                .as_enum()
+                .unwrap()
+                .values()
+                .map(|v| v.name().to_string())
+                .collect::<Vec<_>>();
+            let is_ordered = enum_values.windows(2).all(|w| w[0] <= w[1]);
+            let dict_id = self.dictionaries.add_dictionary(enum_values);
+            Field::new_dict(name, data_type, true, dict_id, is_ordered)
+        } else {
+            Field::new(name, data_type, true)
+        }
+    }
+
+    /// Convert protobuf data type to arrow data type
+    fn kind_to_type(&mut self, kind: prost_reflect::Kind) -> DataType {
+        match kind {
+            prost_reflect::Kind::Double => DataType::Float64,
+            prost_reflect::Kind::Float => DataType::Float32,
+            prost_reflect::Kind::Int32 => DataType::Int32,
+            prost_reflect::Kind::Int64 => DataType::Int64,
+            prost_reflect::Kind::Uint32 => DataType::UInt32,
+            prost_reflect::Kind::Uint64 => DataType::UInt64,
+            prost_reflect::Kind::Sint32 => DataType::Int32,
+            prost_reflect::Kind::Sint64 => DataType::Int64,
+            prost_reflect::Kind::Fixed32 => DataType::UInt32,
+            prost_reflect::Kind::Fixed64 => DataType::UInt64,
+            prost_reflect::Kind::Sfixed32 => DataType::Int32,
+            prost_reflect::Kind::Bool => DataType::Boolean,
+            prost_reflect::Kind::Sfixed64 => DataType::Int64,
+            prost_reflect::Kind::String => DataType::Utf8,
+            prost_reflect::Kind::Bytes => DataType::Binary,
+            prost_reflect::Kind::Message(msg) => {
+                let fields = msg.fields();
+                if fields.len() > 0 {
+                    DataType::Struct(msg.fields().map(|f| self.to_arrow_mut(&f)).collect())
+                } else {
+                    DataType::Boolean
+                }
+            }
+            prost_reflect::Kind::Enum(_) => {
+                let key_type = Box::new(DataType::Int32);
+                let value_type = Box::new(DataType::Utf8);
+                DataType::Dictionary(key_type, value_type)
+            }
+        }
     }
 }
 
-/// Convert protobuf data type to arrow data type
-fn kind_to_type(kind: prost_reflect::Kind) -> DataType {
-    match kind {
-        prost_reflect::Kind::Double => DataType::Float64,
-        prost_reflect::Kind::Float => DataType::Float32,
-        prost_reflect::Kind::Int32 => DataType::Int32,
-        prost_reflect::Kind::Int64 => DataType::Int64,
-        prost_reflect::Kind::Uint32 => DataType::UInt32,
-        prost_reflect::Kind::Uint64 => DataType::UInt64,
-        prost_reflect::Kind::Sint32 => DataType::Int32,
-        prost_reflect::Kind::Sint64 => DataType::Int64,
-        prost_reflect::Kind::Fixed32 => DataType::UInt32,
-        prost_reflect::Kind::Fixed64 => DataType::UInt64,
-        prost_reflect::Kind::Sfixed32 => DataType::Int32,
-        prost_reflect::Kind::Bool => DataType::Boolean,
-        prost_reflect::Kind::Sfixed64 => DataType::Int64,
-        prost_reflect::Kind::String => DataType::Utf8,
-        prost_reflect::Kind::Bytes => DataType::Binary,
-        prost_reflect::Kind::Message(msg) => {
-            let fields = msg.fields();
-            if fields.len() > 0 {
-                DataType::Struct(msg.fields().map(|f| to_arrow(&f)).collect())
-            } else {
-                DataType::Boolean
-            }
-        }
-        prost_reflect::Kind::Enum(_) => {
-            let key_type = Box::new(DataType::Int32);
-            let value_type = Box::new(DataType::Utf8);
-            DataType::Dictionary(key_type, value_type)
-        }
-    }
+/// Dynamically convert protobuf messages to Arrow table or Schema.
+#[derive(Debug, Clone)]
+pub struct SchemaConverter {
+    pub(crate) descriptor_pool: DescriptorPool,
+    /// message name -> dictionary values for the schema
+    dictionary_map: RefCell<HashMap<String, DictValuesContainer>>,
 }
 
 impl SchemaConverter {
     pub fn new(descriptor_pool: DescriptorPool) -> Self {
-        Self { descriptor_pool }
+        let dictionary_map = RefCell::new(HashMap::new());
+        Self { descriptor_pool, dictionary_map }
     }
     /// Compile protobuf files and build the converter.
     ///
@@ -153,7 +188,13 @@ impl SchemaConverter {
             Some(m) => m,
             None => return Ok(None),
         };
-        let schema = Schema::new(msg.fields().map(|f| to_arrow(&f)).collect());
+        let mut field_converter = FieldConverter::new();
+        let schema = Schema::new(
+            msg.fields()
+                .map(|f| field_converter.to_arrow_mut(&f))
+                .collect(),
+        );
+        self.dictionary_map.borrow_mut().insert(name.to_string(), field_converter.dictionaries.clone());
 
         if projection.is_empty() {
             Ok(Some(schema))
@@ -163,6 +204,12 @@ impl SchemaConverter {
             let keep: Vec<Field> = project_fields(&prefix, schema.fields(), &proj_set);
             Ok(Some(Schema::new(keep)))
         }
+    }
+
+    pub fn get_arrow_schema_with_dictionaries(&self, name: &str, projection: &[&str]) -> Result<(Option<Schema>, Option<DictValuesContainer>)> {
+        let rs = self.get_arrow_schema(name, projection)?;
+        let dict_values = self.dictionary_map.borrow_mut().get(name).map(|v| v.clone());
+        Ok((rs, dict_values))
     }
 
     pub fn get_message_by_name(&self, name: &str) -> Result<MessageDescriptor> {
@@ -176,11 +223,12 @@ impl SchemaConverter {
         short_name: &str,
         projection: &[&str],
     ) -> Result<Vec<Option<Schema>>> {
-        self.descriptor_pool
-            .all_messages()
-            .filter(|m| m.name() == short_name)
-            .map(|m| self.get_arrow_schema(m.full_name(), projection))
-            .collect()
+        let descriptors = self.descriptor_pool.all_messages().filter(|m| m.name() == short_name).collect::<Vec<_>>();
+        let mut schemas = Vec::with_capacity(descriptors.len());
+        for m in descriptors {
+            schemas.push(self.get_arrow_schema(m.full_name(), projection)?);
+        }
+        Ok(schemas)
     }
 
     pub fn get_messages_from_short_name(&self, short_name: &str) -> Vec<Option<MessageDescriptor>> {
@@ -223,9 +271,7 @@ fn project_fields(prefix: &str, fields: &Vec<Field>, projection: &HashSet<&str>)
     keep
 }
 
-
 //         let values = f.kind().as_enum().unwrap().values().map(|v| v.name().to_string()).collect::<Vec<_>>();
-
 
 #[cfg(test)]
 mod tests {
@@ -237,8 +283,14 @@ mod tests {
         let mut holder = DictValuesContainer::new();
         assert!(holder.get_dict_values(0).is_none());
         assert_eq!(holder.add_dictionary(vec!["a".to_string()]), 1);
-        assert_eq!(holder.get_dict_values(1).unwrap().iter().map(|v| v.unwrap().to_string()).collect::<Vec<_>>(),
-                   vec!["a".to_string()]);
+        assert_eq!(
+            holder
+                .get_dict_values(1)
+                .unwrap()
+                .iter()
+                .map(|v| v.unwrap().to_string())
+                .collect::<Vec<_>>(),
+            vec!["a".to_string()]
+        );
     }
-
 }
