390a284055c2f8a8b6617f237c065b539bf6ca9d
Dennis Collinson <dennis@impulselabs.com>
2023-03-22 11:00:02 -0400

    Make public LoopJoinSet type

--- katniss-ingestor/src/ingestors/temporal_parquet_ingestor.rs
+++ katniss-ingestor/src/ingestors/temporal_parquet_ingestor.rs
@@ -13,17 +13,20 @@ use crate::{
 };
 use katniss_pb2arrow::{exports::DynamicMessage, ArrowBatchProps};
 
+/// Set Of Tokio Tasks that never return unless they error
+pub type LoopJoinset = JoinSet<Result<Infallible>>; // (Infallible used in place of !)
+
 /// Start a pipeline that ingests dynamic messages to parquet filesystem
 /// Returns:
 /// * a channel that functions as the head of the pipeline
-/// * A JoinSet of infinte loops (Infallible used in place of !) for
+/// * A Set of Infinite Loop Futures for:
 ///     - ArrowEncoding
 ///     - ParquetEncoding
 ///     - Disk access
 pub async fn parquet_fs_pipeline<P: AsRef<Path>>(
     props: ArrowBatchProps,
     directory: P,
-) -> Result<(UnboundedSender<DynamicMessage>, JoinSet<Result<Infallible>>)> {
+) -> Result<(UnboundedSender<DynamicMessage>, LoopJoinset)> {
     let (head, rx_msg) = unbounded_channel();
     let (mut rotator, rx_buf) = TemporalRotator::try_new(rx_msg, &props, Utc::now())?;
     let (mut converter, rx_bytes) = ParquetConverter::new(rx_buf, props.schema);
