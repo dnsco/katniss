77828027aca604bdc262f27c05fff56ec052cffa
Matt Olenick <molenick@impulselabs.com>
2023-07-13 15:59:18 -0700

    Display initial powered_time_secs value on screen

--- katniss-test/src/bin/spaceship-gui.rs
+++ katniss-test/src/bin/spaceship-gui.rs
@@ -8,6 +8,10 @@ use katniss_test::protos::spacecorp::JumpDriveStatus;
 fn main() -> Result<(), Box<dyn Error>> {
     let status = Arc::new(Mutex::new(JumpDriveStatus::default()));
 
+    let main_window = MainWindow::new().unwrap();
+    main_window.set_powered_time_secs(status.lock().unwrap().powered_time_secs);
+    main_window.run().unwrap();
+
     let sim_status = status.clone();
     thread::spawn(move || loop {
         let mut status = sim_status.lock().unwrap();
@@ -22,6 +26,32 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     thread::sleep(Duration::from_secs(3));
     dbg!(status.lock().unwrap().powered_time_secs);
-    /// do slint stuff here
+
+    // do slint stuff here
+
     Ok(())
+}
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        width: 800px;
+        height: 600px;
+
+        in property <int> powered_time_secs;
+
+        Text {
+            text: "Jumpdrive Status";
+            font-size: 24px;
+            y: 12px;
+
+            Text {
+                text: powered_time_secs;
+                font-size: 24px;
+                // manually sizing this space for now, must we do all
+                // positioning math are there alignment concepts like
+                // in swiftui/jetpack (vstack, hstack, etc)?
+                y: 36px;
+            }
+        }
+    }
 }
